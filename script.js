f91fa415d0a3b223026ffd4359e878b7f63726c2
AnkitSharma136 <bt1362023@gmail.com>
2023-09-14 01:14:08 +0530

    added recipes and styling

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 const searchBox = document.querySelector('.searchBox');
 const searchButton =document.querySelector('.searchButton');
 const recipeContainer = document.querySelector('.recipe-container');
+const recipeDetailsContent = document.querySelector('.recipe-details-content');
+const recipeCloseButton =document.querySelector('.recipe-close-button');
 
-searchButton.addEventListener('click' , () =>{
-    console.log("Button Click");
+const fetchRecipes = async (query) => {
+    recipeContainer.innerHTML ="<h2>Fecting recipe ....</h2>";
+    const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+    const response = await data.json();
+
+    //console.log(response);
+    recipeContainer.innerHTML="";
+    response.meals.forEach(meal =>{
+        const recipeDiv = document.createElement('div');
+        recipeDiv.classList.add('recipe');
+        recipeDiv.innerHTML = `
+        <img src="${meal.strMealThumb}">
+        <h3>${meal.strMeal}</h3>
+        <p>${meal.strArea} Dish </p>
+        <p>${meal.strCategory}<p>
+        `
+        const button = document.createElement('button');
+        button.textContent ="View Recipe";
+        recipeDiv.appendChild(button);
+
+        button.addEventListener('click' , () => {
+            openRecipePopup(meal);
+        });
+
+        recipeContainer.appendChild(recipeDiv);
+    })
+};
+
+
+searchButton.addEventListener('click' , (e) =>{
+    e.preventDefault();
+    const searchInput = searchBox.value.trim();
+    fetchRecipes(searchInput);
+    //console.log("Button Click");
 });
